16fcbcb7e597d10e8aa14e00c3e96ac7a7318656
adamKellyObare <aobarekelly@gmail.com>
2023-08-02 11:54:54 +0300

    Structured Landing Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import React from 'react';
+import NavBar from './NavBar';
+import './App.css';
+
 const App = () => {
   return (
-    <div>App</div>
+    <div > 
+      <div className='bg'>
+      </div>
+
+        <main> 
+        <NavBar />
+      <h1>Major League Soccer</h1>
+      <div className='content'>
+        
+      
+      </div>
+      
+    </main>
+    
+    </div>
+    
   )
 }
+
 export default App
---
+++ src/Teams.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import backgroundImage from './background.jpg';
+
+export default function Teams() {
+  return (
+    <div>
+      <h2>Teams</h2>
+      {/* The Teams Components to be put here*/}
+    </div>
+  );
+}

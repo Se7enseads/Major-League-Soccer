d757f3033ee78a57713a067d020eb07c6471a8dd
Kyle Mututo <135003824+Se7enseads@users.noreply.github.com>
2023-08-03 18:12:47 +0300

    Completed Task.

--- src/peter/CollectionOfLeagues.js
+++ src/peter/CollectionOfLeagues.js
@@ -1,61 +1,36 @@
-import React from "react";
+import React from 'react'
 
 const leagues = [
-  // ... (your league data)
-  { name: "epl", logo: "https://static.vecteezy.com/system/resources/previews/010/994/451/original/premier-league-logo-symbol-with-name-design-england-football-european-countries-football-teams-illustration-with-purple-background-free-vector.jpg" },
-  { name: "bundesliga", logo: "https://upload.wikimedia.org/wikipedia/en/thumb/d/df/Bundesliga_logo_%282017%29.svg/1200px-Bundesliga_logo_%282017%29.svg.png" },
-  { name: "La Liga", logo: "https://www.fifplay.com/img/public/laliga-logo.jpg" },
-  { name: "Seria A", logo: "https://www.fifplay.com/img/public/serie-a-logo.jpg" },
-  { name: "Europa", logo: "https://upload.wikimedia.org/wikipedia/en/thumb/7/77/UEFA_Europa_League_%28football_competition%29_logo.svg/1200px-UEFA_Europa_League_%28football_competition%29_logo.svg.png" },
-  { name: "UEFA", logo: "https://media.asroma.com/prod/images/gm_preview/eb2b41232410-keyviewsueclrb8zmf.jpg" },
-  { name: "MLS", logo: "https://cdn.freebiesupply.com/images/large/2x/mls-logo-black-and-white.png" },
-  { name: "UCL", logo: "https://1.bp.blogspot.com/-aqbEPSi6fBs/YQlhDOFZRYI/AAAAAAADKdo/C7YepMYAQcMzC2vwW-r8i5n2mLIQosXWACNcBGAsYHQ/s1000/uefa-champions.league-21-22-logo%2B%25282%2529.jpg" },
-  
-];
+  { name: 'Premier League', logo: 'https://crests.football-data.org/PL.png' },
+  { name: 'Bundesliga', logo: 'https://crests.football-data.org/BL1.png' },
+  { name: 'LA Liga', logo: 'https://crests.football-data.org/PD.png' },
+  { name: 'Serie A', logo: 'https://crests.football-data.org/SA.png' },
+  { name: 'Ligue 1', logo: 'https://crests.football-data.org/FL1.png' }
+]
 
-const backgroundImageUrl = "url('https://images.pexels.com/photos/11849230/pexels-photo-11849230.jpeg?auto=compress&cs=tinysrgb&w=1600')";  
-
-const LeaguesType = ({ table }) => {
-  const containerStyle = {
-    height: '100vh',
-    backgroundImage: backgroundImageUrl,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'red', // Set the background color to red
-  };
-
-  const titleStyle = {
-    fontSize: '3rem', // Increase the font size to be bigger
-    fontWeight: 'bold', // Set the font weight to bold
-    textAlign: 'center', // Center the text horizontally
-    color: 'white', // Set the text color to white for better visibility on a red background
-  };
-
-  const logoStyle = {
-    width: '50px',
-    height: '50px',
-  };
-
+const LeaguesType = () => {
   return (
-    <div style={containerStyle}>
-      <div className="container">
-        <div className="col">
-          <h1 className="display-4" style={titleStyle}>LEAGUES PAGE</h1> {/* Apply the updated titleStyle */}
-          <div className="btn-group-vertical">
-            {leagues.map((league, index) => (
-              <button key={index} className="btn btn-block btn-primary">
-                <img src={league.logo} alt={`${league.name} logo`} className="mr-2" style={logoStyle} />
-                {league.name}
-              </button>
-            ))}
-          </div>
+    <div className='container-fluid'>
+      <h1 className='text-center'>LEAGUES PAGE</h1>
+      <div className='d-flex justify-content-center align-items-center'>
+        <div className='mx-auto'>
+          {leagues.map((league, index) => (
+            <div
+              key={index}
+              className='btn d-flex btn-primary mb-3 w-100 justify-content-between'
+            >
+              <img
+                src={league.logo}
+                alt={`${league.name} logo`}
+                height={'50px'}
+              />
+              <h2 className='text-dark'>{league.name}</h2>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LeaguesType;
+export default LeaguesType
